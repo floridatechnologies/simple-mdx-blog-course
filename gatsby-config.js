b41be9c04747bd6ac51eab061fc3361c66d347b1
floridatecnhologies <gavilanesisrael8f@gmail.com>
2021-07-26 22:33:18 -0400

    add mdx plugin and sharp library

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,23 @@ module.exports = {
         path: '${__dirname}/src/images'
       },
     },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins:[
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxwidth:1200,
+
+            },
+          },
+        ],
+      },
+    },
+
   ],
 }
