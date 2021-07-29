799623900b346ed1970afe2f9850f83c429d6716
floridatecnhologies <gavilanesisrael8f@gmail.com>
2021-07-29 19:59:57 -0400

    added container components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,52 @@
-import React from "react"
+// import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+// export default function Home() {
+//   return <div>Hello world!</div>
+// }
+
+import React from "react"
+import { Container } from "../components"
+
+export default function Home(){
+    return (
+    <Container>
+      <h1>Hello-World</h1>
+    </Container>
+  )
 }
+
+// }
+
+// export default IndexPage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react"
+// import { Container } from "../components"
+
+// const IndexPage = () => {
+//   return (
+//     <Container>
+//       <h1>Hello-World</h1>
+//     </Container>
+//   )
+// }
+
+// export default IndexPage
